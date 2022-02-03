4ca4be1faa48bb2fc621b4addc8b6dd7ce889cba
Pooja9586 <r.pooja9586@gmail.com>
2022-02-03 22:15:54 +0530

    program to print pattern matrix
    program that takes user's input for size and elements of matrix and prints the elements in matrix format

---
+++ patternMatrix.c
@@ -0,0 +1,31 @@
+//program to print matrix of pattern
+
+#include<stdio.h>
+
+void main()
+{
+    int m,n,i,j;
+    
+    printf("add size of matrix\n") ;
+    printf("number of rows = ") ;
+    scanf("%d",&m) ;
+    printf("number of columns = ") ;
+    scanf("%d",&n) ;
+    char arr[m][n] ;
+    int size = m * n ;
+    printf("Enter %d elements of matrix \n", size) ;
+    for(i=0; i<m; i++)    
+        for(j=0;j<n;j++)
+            scanf("%s",&arr[i][j]) ;
+
+    printf("\n") ;
+    printf("The matrix is \n") ;    
+    for(i=0; i<m; i++)    
+    {
+        for(j=0;j<n;j++)
+            printf("%c\t",arr[i][j]) ;
+         printf("\n") ;
+    }
+        
+
+}
